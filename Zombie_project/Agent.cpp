f8b6eb8f7e9ab00679ce2630131a14138b5f24a6
Weikang01 <lwkdota@qq.com>
2021-05-28 21:54:51 -0400

    fixed bug of tile collision

--- Zombie_project/Agent.cpp
+++ Zombie_project/Agent.cpp
@@ -85,7 +85,7 @@ void Agent::collideWithTile(glm::vec2 tilePos)
 	float yDepth = MIN_DISTANCE - abs(distVec.y);
 
 
-	if (xDepth > 0 || yDepth > 0)
+	if (xDepth > 0 && yDepth > 0)
 	{
 		if (std::max(xDepth, 0.f) < std::max(yDepth, 0.f))
 			if (distVec.x < 0)
